d43026ce4a9a24f204018c7aeea3c56d6e15c3d9
GennadiyMarin <64727554+gennadiymarin@users.noreply.github.com>
2024-04-05 11:29:26 +0300

    CityScapes Dataset

---
+++ data_sets.py
@@ -0,0 +1,26 @@
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from PIL import Image
+import torchvision.transforms.functional as F
+
+
+class CityScapesDataSet(Dataset):
+    def __init__(self, src_data: str, train=True):
+        self.path = src_data
+        self.path += '/train/' if train else '/val/'
+        self.length = 2975 if train else 500
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, item: int):
+        img = F.pil_to_tensor(Image.open(self.path + str(item + 1) +'.jpg'))
+        return img[:,:,:256], img[:,:,256:]
+
+
+# train_set = CityScapesDataSet('/kaggle/input/cityscapes-image-pairs/cityscapes_data')
+# val_set = CityScapesDataSet('/kaggle/input/cityscapes-image-pairs/cityscapes_data', train=False)
+#
+# train_loader = DataLoader(train_set, batch_size=CFG.batch_size, shuffle=True)
+# val_loader = DataLoader(val_set, batch_size=CFG.batch_size, shuffle=False)
